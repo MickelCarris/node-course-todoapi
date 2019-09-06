dc4e82f21caed569792737f842b8f9b4ee3617ac
MickelCarris <mickelcarris0007@gmail.com>
2019-09-06 16:56:41 +0530

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.post('/todos', (req, res) => {
   );
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => res.send(e)
+  );
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
